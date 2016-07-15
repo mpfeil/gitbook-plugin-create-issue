0eb212c081f0c7c432112b45ccc03da4900f99e5
Matthias Pfeil <matthias.pfeil@wwu.de>
2016-07-15 15:47:22 +0200

    new config settings

--- assets/issue.js
+++ assets/issue.js
@@ -3,10 +3,14 @@ require(['gitbook'], function (gitbook) {
 
     var conf = config["create-issue"];
     var label = conf.label;
+    var labelSubmit = conf.labelSubmitButton;
+    var labelCancel = conf.labelCancelButton;
     var lang = gitbook.state.innerLanguage;
     if (lang) {
       // label can be a unique string for multi-languages site
       if (typeof label === 'object') label = label[lang];
+      if (typeof labelSubmit === 'object') labelSubmit = labelSubmit[lang];
+      if (typeof labelCancel === 'object') labelCancel = labelCancel[lang];
 
       lang = lang + '/';
     }
@@ -18,20 +22,22 @@ require(['gitbook'], function (gitbook) {
       cssClass: ['custom-class-1', 'custom-class-2'],
       onOpen: function() {
           console.log('modal open');
+          //TODO maybe set title
       },
       onClose: function() {
           console.log('modal closed');
+          //TODO submit new issue
       }
     });
 
     // add a button
-    modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
+    modal.addFooterBtn(labelSubmit, 'tingle-btn tingle-btn--primary', function() {
         // here goes some logic
         modal.close();
     });
 
     // add another button
-    modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
+    modal.addFooterBtn(labelCancel, 'tingle-btn tingle-btn--danger', function() {
         // here goes some logic
         modal.close();
     });
